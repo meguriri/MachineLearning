639d4ef8a76acff4cdca6bdce5b497e47ec93a42
meguriri <1310500986@qq.com>
2024-11-06 19:57:00 +0800

    add note

--- tree/__init__.py
+++ tree/__init__.py
@@ -7,17 +7,21 @@ import pickle
 import random
 
 # 特征标签
-labels = ['age', 'workclass', 'fnlwgt', 'education', 'education-num',
-              'marital-status', 'occupation',
-              'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week',
-              'native-country']
+labels = ['age', 'workclass', 'fnlwgt', 
+          'education', 'education-num',
+            'marital-status', 'occupation',
+            'relationship', 'race', 
+            'sex', 'capital-gain', 
+            'capital-loss', 'hours-per-week',
+            'native-country']
 # 特征的类型
-labelType = ['continuous', 'uncontinuous', 'continuous',
-                 'uncontinuous',
-                 'continuous', 'uncontinuous',
-                 'uncontinuous', 'uncontinuous', 'uncontinuous',
-                 'uncontinuous', 'continuous', 'continuous',
-                 'continuous', 'uncontinuous']
+labelType = ['continuous', 'uncontinuous', 
+            'continuous','uncontinuous',
+            'continuous', 'uncontinuous',
+            'uncontinuous', 'uncontinuous', 
+            'uncontinuous','uncontinuous', 
+            'continuous', 'continuous',
+            'continuous', 'uncontinuous']
 
 # 训练决策树
 def train(dataset, labels, labelType):
@@ -26,64 +30,86 @@ def train(dataset, labels, labelType):
 
 # 单个决策树的测试
 def test(tree, testFilePath, labels, labelType, dataset):
+    # 读取测试集
     testDataset = pd.read_csv(testFilePath, header=None, sep=', ',engine='python')
     testDataset = testDataset[~testDataset.isin(['?']).any(axis=1)]
     testDataset = testDataset.values.tolist()
+    # 测试集的数据个数
     total = len(testDataset)
+    # 正确与错误的个数
     correct = 0
     error = 0
-    i = 1
+    # 循环读取测试集的每一条测试数据
     for line in testDataset:
-        print(i)
+        # 通过决策树对数据进行分类
         result = classify(tree, line, labels, labelType) + '.'
+        # 与测试集数据标签一致
         if result == line[-1]:
             correct += 1
+        # 与测试集数据标签不一致
         else:
             print('{} is error;result:{},correct:{}'.format(line, result, line[-1]))
             error += 1
-        i += 1
-    print('load {} lines data'.format(total))
+    # 输出正确错误个数，以及正确率
     print('Correct: {},Error: {},Accuracy: {}'.format(correct, error, correct / total))
 
 
 # 随机森林测试
 def forestTest(forest):
+    # 读取测试集
     testDataset = pd.read_csv('./adult/adult.test', header=None, sep=', ',engine='python')
     testDataset = testDataset[~testDataset.isin(['?']).any(axis=1)]
     testDataset = testDataset.values.tolist()
+    # 正确与错误的个数
     correct = 0
     error = 0
+    # 循环读取测试集的每一条测试数据
     for i in range(len(testDataset)):
+        # 记录每棵树的分类
         c1 = 0
         c2 = 0
+        # 循环读取每一棵决策树
         for tree in forest:
+            # 获取当前这棵树的分类
             classLabel = classify(tree,testDataset[i],labels,labelType)+'.'
+            # 统计每种分类的个数
             if classLabel == '<=50K.':
                 c1+=1
             else:
                 c2+=1
+        # 选择每棵树分类结果的众数作为随机森林的分类
         answer = '<=50K.' if c1 > c2 else '>50K.'
+        # 与测试集数据标签一致
         if answer == testDataset[i][-1]:
             correct += 1
+        # 与测试集数据标签不一致
         else:
             error += 1
+    # 输出正确错误个数，以及正确率
     print('Correct: {},Error: {},Accuracy: {:.2f}'.format(correct, error, correct / len(testDataset)))
 
 # 创建随机采样的数据集
 def createSampleDataset(filename,batchSize):
+    # 指定随机选取多少个特征
     random_numbers = random.sample(range(0, len(labelType)), 5)
+    # 读取数据集
     dataset = pd.read_csv(filename, header=None, sep=', ',engine='python')
+    # 清洗数据
     dataset = dataset[~dataset.isin(['?']).any(axis=1)]
+    # 随机选取n条数据作为训练集
     dataset = dataset.sample(n=batchSize)
     dataset = dataset.values.tolist()
     sampleDataset = [[row[i] for i in random_numbers] for row in dataset]
+    # 将分类标签加入数据集
     for i in range(len(sampleDataset)):
         sampleDataset[i].append(dataset[i][-1])
+    # 记录选择的特征标签和特征标签类型
     sampleLabels = []
     sampleLabelType = []
     for i in random_numbers:
         sampleLabels.append(labels[i])
         sampleLabelType.append(labelType[i])
+    # 返回数据集和数特征标签
     return sampleDataset, sampleLabels, sampleLabelType
 
 # 将决策树存储到本地
--- tree/dtree.py
+++ tree/dtree.py
@@ -1,64 +1,97 @@
+'''
+    dtree.py
+    C4.5决策树的构建等函数
+'''
 from math import log2
 import operator
-import pickle
 import pandas as pd
 
 
+# 读取全部数据集
 def createDataset(filename):
+    # 读取数据集
     dataset = pd.read_csv(filename, header=None, sep=', ',engine='python')
+    # 清洗数据集
     dataset = dataset[~dataset.isin(['?']).any(axis=1)]
     dataset = dataset.values.tolist()
+    # 特征标签
     labels = ['age', 'workclass', 'fnlwgt', 'education', 'education-num',
               'marital-status', 'occupation',
               'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week',
               'native-country']
+    # 特征的类型
     labelType = ['continuous', 'uncontinuous', 'continuous',
                  'uncontinuous',
                  'continuous', 'uncontinuous',
                  'uncontinuous', 'uncontinuous', 'uncontinuous',
                  'uncontinuous', 'continuous', 'continuous',
                  'continuous', 'uncontinuous']
+    # 返回数据集
     return dataset, labels, labelType
 
 
-
+# 计算信息熵
 def calculateEnt(dataset):
+    # 分类统计
     classCount = {}
     n = len(dataset)
+    # 遍历数据集
     for vec in dataset:
+        # 获取当前数据的分类
         classification = vec[-1]
+        # 更新classCount
         if classification not in classCount.keys():
             classCount[classification] = 0
         classCount[classification] += 1
+    # 计算信息熵
     ent = 0.0
     for key in classCount:
+        # 当前分类的占比
         p = classCount[key] / n
+        # 信息熵
         ent += -1 * (p * log2(p))
+    # 返回信息熵
     return ent
 
+# 通过离散型特征划分数据集
 def splitDataset(dataset, labelIdx, value):
     newDataset = []
+    # 遍历数据集
     for vec in dataset:
+        # 当前数据的特征等于选择的特征
         if vec[labelIdx] == value:
+            # 剔除掉当前特征
             tmp = vec[:labelIdx]
             tmp.extend(vec[labelIdx + 1:])
+            # 将该数据加入新的数据集
             newDataset.append(tmp)
+    # 返回划分后的数据集
     return newDataset
 
-
+# 通过连续型特征划分数据集
 def splitContinuousDataset(dataset, labelIdx, value):
+    # 大于分割点的数据集
     biggerDataset = []
+    # 小于等于分割点的数据集
     smallerDataset = []
+    # 遍历数据集
     for vec in dataset:
+        # 剔除掉当前特征
         tmp = vec[:labelIdx]
         tmp.extend(vec[labelIdx + 1:])
+        # 如果当前特征值大于分割点
         if float(vec[labelIdx]) > value:
+            # 当前数据加入大于分割点的数据集
             biggerDataset.append(tmp)
+        # 如果当前特征值小于等于分割点
         else:
+            # 当前数据加入小于等于分割点的数据集
             smallerDataset.append(tmp)
+    # 返回两个划分后的新的数据集
     return biggerDataset, smallerDataset
 
 
+# 计算离散型特征的信息增益比
 def calGainRatioUnContinuous(dataset, labelIdx, ent):
     # 获取该特征的类别
     featureList = [vec[labelIdx] for vec in dataset]
@@ -73,15 +106,16 @@ def calGainRatioUnContinuous(dataset, labelIdx, ent):
         D = len(subDataset) / len(dataset)
         iv += -1 * (D * log2(D))
         entc += D * calculateEnt(subDataset)
+    # 信息增益
     gain = ent - entc
     # 全是一个类别split = 1
     if iv == 0:
         iv = 1
-    # 计算gainRatio
+    # 计算信息增益率
     gainRadio = gain / iv
     return gainRadio
 
-
+# 计算连续型特征的信息增益比
 def calGainRatioContinuous(dataset, labelIdx, ent):
     # 获取该连续特征的各种值
     valueList = [float(vec[labelIdx]) for vec in dataset]
@@ -98,12 +132,15 @@ def calGainRatioContinuous(dataset, labelIdx, ent):
     for i in range(len(splitPoint)):
         entc = 0.0
         iv = 0.0
+        # 获取当前分割点后的划分的两个数据集
         biggerDataset, smallerDataset = splitContinuousDataset(dataset, labelIdx, splitPoint[i])
         Db = len(biggerDataset) / len(dataset)
         Ds = len(smallerDataset) / len(dataset)
+        # 计算当前划分的信息增益
         entc += Db * calculateEnt(biggerDataset)
         entc += Ds * calculateEnt(smallerDataset)
         gain = ent - entc
+        # 更新最优的划分点
         if gain > bestGain:
             bestGain = gain
             iv += -1 * (Db * log2(Db))
@@ -112,6 +149,7 @@ def calGainRatioContinuous(dataset, labelIdx, ent):
             # TODO(meguriri): 修正信息增益
             # 计算最优信息增益率
             bestGainRatio = (log2(len(valueList) - 1) / abs(len(dataset))) / iv
+    # 返回最优信息增益率和最优分割点
     return bestGainRatio, bestSplitPoint
 
 
@@ -126,14 +164,17 @@ def dict2list(dic: dict):
 # 特征用完后，叶子节点未分类成功，选择出现次数最多的分类
 def majority(classList):
     classficationCount = {}
+    # 遍历分类列表
     for i in classList:
         if i not in classficationCount.keys():
             classficationCount[i] = 0
         classficationCount[i] += 1
+    # 排序选择最多的分类作为当前叶子节点的分类
     sortedClassCount = sorted(dict2list(classficationCount), key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
 
+# 选择当前分支的最优的特征
 def chooseBestSplit(dataset, labelType):
     # 选取的特征是否连续
     isContinuous = False
@@ -144,70 +185,95 @@ def chooseBestSplit(dataset, labelType):
     bestSplitPoint = 0.0
     # 遍历特征
     for featureIdx in range(len(dataset[0]) - 1):
+        # 当前的特征是离散的
         if labelType[featureIdx] == 'uncontinuous':
+            # 计算当前特征的信息增益率
             gainRatio = calGainRatioUnContinuous(dataset, featureIdx, ent)
+            # 更新最优的特征和信息增益率
             if gainRatio > bestGainRatio:
                 bestGainRatio = gainRatio
                 bestFeatureIdx = featureIdx
                 isContinuous = False
+         # 当前的特征是连续的
         else:
+            # 计算当前特征的信息增益率和划分点
             gainRatio, splitPoint = calGainRatioContinuous(dataset, featureIdx, ent)
+            # 更新最优的特征和信息增益率
             if gainRatio > bestGainRatio:
                 bestGainRatio = gainRatio
                 bestFeatureIdx = featureIdx
                 bestSplitPoint = splitPoint
                 isContinuous = True
+    # 返回最优特征和最优划分点和该特征是离散的还是连续的
     return bestFeatureIdx, bestSplitPoint, isContinuous
 
-
+# 构建C4.5决策树
 def createTree(dataset, labels, labelType):
     # 递归出口
     # 构造叶子节点分类
     classificationList = [vex[-1] for vex in dataset]
+    # 如果当前只有一种分类，结束递归。选择当前分类作为当前叶子节点的分类
     if classificationList.count(classificationList[0]) == len(classificationList):
         print('classificate is ok', classificationList[0])
         return classificationList[0]
+    # 如果当前没有可以分类的特征
     if len(dataset[0]) == 1:
+        # 选择出现最多的分类作为当前叶子节点的分类
         print('no features', majority(classificationList))
         return majority(classificationList)
+    # 选择最优特征和划分点
     bestFeatureIdx, bestSplitPoint, isContinuous = chooseBestSplit(dataset, labelType)
-    if bestFeatureIdx == -1:  # gain等于0
+    # 未选出（说明当前的分类全是一样的数据，信息增益为0）
+    if bestFeatureIdx == -1:
+        # 选择出现最多的分类作为当前叶子节点的分类
         return majority(classificationList)
     bestFeature = labels[bestFeatureIdx]
     print(bestFeature)
     tree = {bestFeature: {}}
+    # 更新特征标签和特征类型列表
     del (labels[bestFeatureIdx])
     del (labelType[bestFeatureIdx])
+    # 选择的特征是连续的
     if isContinuous:
+        # 获取划分点划分的两个数据集
         biggerDataset, smallerDataset = splitContinuousDataset(dataset, bestFeatureIdx, bestSplitPoint)
+        # 递归建树
         subLabels = labels[:]
         subLabelType = labelType[:]
         tree[bestFeature]['>' + str(bestSplitPoint)] = createTree(biggerDataset, subLabels, subLabelType)
         subLabels = labels[:]
         subLabelType = labelType[:]
         tree[bestFeature]['<=' + str(bestSplitPoint)] = createTree(smallerDataset, subLabels, subLabelType)
+    # 选择的特征是离散的
     else:
-        # 获取最优特征的全部类别
+        # 获取最优特征的全部种类
         featureList = [vex[bestFeatureIdx] for vex in dataset]
         uniqueFeature = set(featureList)
+        # 对每个种类递归建树
         for feature in uniqueFeature:
             subLabels = labels[:]
             subLabelType = labelType[:]
             subDataset = splitDataset(dataset, bestFeatureIdx, feature)
             tree[bestFeature][feature] = createTree(subDataset, subLabels, subLabelType)
-
+    # 返回决策树
     return tree
 
 
 # 分类任务，在树上根据data的特征
 def classify(tree, data, labels, labelType):
+    # 获取树的第一个特征选择
     feature = list(tree.keys())[0]
+    # 获取树的子节点
     dic = tree[feature]
+    # 获取该特征对应的index
     featureIdx = labels.index(feature)
+    # 初始化选择的分类
     classLabel = '<=50k'
-
-    if labelType[featureIdx] == 'uncontinuous':  # 非连续特征
+    # 非连续特征
+    if labelType[featureIdx] == 'uncontinuous':  
+        # 获取当前特征值
         nowFeature = data[featureIdx]
+        # 寻找分支
         for key in dic.keys():
             if key == nowFeature:
                 # 非叶子结点
@@ -217,14 +283,18 @@ def classify(tree, data, labels, labelType):
                 else:
                     classLabel = dic[key]
                 break
-    else:  # 连续特征
+    # 连续特征
+    else:  
+        # 获取当前特征值
         nowFeature = float(data[featureIdx])
         firstBranch = list(dic.keys())[0]
         splitPoint = ''
+        # 获取当前的分割点数值
         if str(firstBranch).startswith('>'):
             splitPoint = firstBranch[1:]
         else:
             splitPoint = firstBranch[2:]
+        # 特征值与当前的分割点比较
         if nowFeature > float(splitPoint):
             # 非叶子结点
             if type(dic['>' + str(splitPoint)]).__name__ == 'dict':
@@ -239,4 +309,5 @@ def classify(tree, data, labels, labelType):
             # 叶子结点
             else:
                 classLabel = dic['<=' + str(splitPoint)]
+    # 返回类别           
     return classLabel
